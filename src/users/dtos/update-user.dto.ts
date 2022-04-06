83b4915b5eb5b5072e4c28e5844c4493c13a2fb4
Wojciech Dudek <wdudek82@gmail.com>
2022-04-07 00:05:46 +0200

    Add UpdateUserDto

--- src/users/dtos/update-user.dto.ts
+++ src/users/dtos/update-user.dto.ts
@@ -1,3 +1,23 @@
-export class UpdateUserDto {
+import { IsEmail, IsEnum, IsOptional, IsString } from 'class-validator';
+import { Role } from '../enums';
 
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @IsEnum(Role)
+  @IsOptional()
+  role: Role;
+
+  // TODO: Implement double check, i.e.
+  //  when password is changed, it has to be typed twice.
+  //  And then both password fields are required.
+  @IsString()
+  @IsOptional()
+  password: string;
 }
