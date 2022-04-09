1d7fc171cf362f2bb033a5cf851d8a8bdfb9064a
Wojciech Dudek <wdudek82@gmail.com>
2022-04-09 18:36:05 +0200

    Add docker-compose config for postgres database

--- src/users/services/auth.service.ts
+++ src/users/services/auth.service.ts
@@ -4,7 +4,6 @@ import {
   BadRequestException,
   Injectable,
   NotFoundException,
-  NotImplementedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
