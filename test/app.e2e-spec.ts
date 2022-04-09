0ffb63173efaac31dba662dbbd4040f9e53653b6
Wojciech Dudek <wdudek82@gmail.com>
2022-04-09 19:01:58 +0200

    Add e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,22 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+    const password = 'asdf';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
