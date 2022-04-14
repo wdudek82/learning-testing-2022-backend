534b6d159c86ae59b1a3d4007bc2a6abf2b33db2
Wojciech Dudek <wdudek82@gmail.com>
2022-04-14 22:31:22 +0200

    Update cookie config #11

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      // 'http://localhost:4200',
+      'http://localhost:4200',
       'https://wdudek82.github.io',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
