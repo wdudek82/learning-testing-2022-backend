cae303e800d9b0648a92037fbecda60d8e2022c4
Wojciech Dudek <wdudek82@gmail.com>
2022-04-15 01:30:16 +0200

    Heroku deployment with angular served statically #8

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,8 @@ function setupSwagger(app: INestApplication): void {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://wdudek82.github.io'],
+    // origin: ['http://localhost:4200', 'https://wdudek82.github.io'],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
