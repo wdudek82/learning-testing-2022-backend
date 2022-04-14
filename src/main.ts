ea36ff4412625ec40c7804d215e256fb8136643f
Wojciech Dudek <wdudek82@gmail.com>
2022-04-14 10:20:01 +0200

    Amend CORS settings #2

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ function setupSwagger(app: INestApplication): void {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://wdudek82.github.io:80'],
+    origin: ['http://localhost:4200', 'https://wdudek82.github.io'],
     credentials: true,
   });
   app.use(
