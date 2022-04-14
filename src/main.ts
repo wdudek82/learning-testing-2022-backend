a20e15b156b830fcf844439b864aa504c2bb0a8e
Wojciech Dudek <wdudek82@gmail.com>
2022-04-15 01:25:37 +0200

    Heroku deployment with angular served statically #7

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
     cookieSession({
       keys: ['key1'],
       // sameSite: 'none',
-      secure: true,
+      // secure: true,
       // httpOnly: false,
     }),
   );
