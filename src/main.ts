179bfa71369fd3e5ef44c0afb552c39604166ac7
Wojciech Dudek <wdudek82@gmail.com>
2022-04-14 10:25:49 +0200

    Amend CORS settings #3

--- src/main.ts
+++ src/main.ts
@@ -20,13 +20,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:4200', 'https://wdudek82.github.io'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  app.use(
-    cookieSession({
-      keys: ['foo'],
-    }),
-  );
+  app.use(cookieSession({}));
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
