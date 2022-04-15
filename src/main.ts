3960a20af8430550e53ce36e86f39d0f857235e2
Wojciech Dudek <wdudek82@gmail.com>
2022-04-15 20:17:23 +0200

    Change Cookie settings #15 switch to express-session

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const cookieSession = require('cookie-session');
+const session = require('express-session');
+// const cookieSession = require('cookie-session');
 
 function setupSwagger(app: INestApplication): void {
   const config = new DocumentBuilder()
@@ -17,7 +19,7 @@ function setupSwagger(app: INestApplication): void {
 }
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: ['http://localhost:4200', 'https://wdudek82.github.io'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
@@ -30,17 +32,28 @@ async function bootstrap() {
     ],
     credentials: true,
   });
+  // app.set('trust proxy', 1);
   app.use(
-    cookieSession({
-      keys: ['key1'],
-      secret: 'aVerySecretPhrase',
-      sameSite: 'none',
-      secure: true,
-      domain: 'https://wdduek82.github.io',
-      path: '/',
-      maxAge: 1000 * 60 * 60, // 1 hour
+    session({
+      secret: 'my-secret',
+      cookie: {
+        sameSite: 'none',
+        secure: true,
+      },
     }),
   );
+  // app.use(
+  //   cookieSession({
+  //     name: 'session',
+  //     keys: ['key1'],
+  //     secret: 'aVerySecretPhrase',
+  //     sameSite: 'none',
+  //     secure: true,
+  //     domain: 'https://wdduek82.github.io',
+  //     path: '/',
+  //     maxAge: 1000 * 60 * 60, // 1 hour
+  //   }),
+  // );
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
